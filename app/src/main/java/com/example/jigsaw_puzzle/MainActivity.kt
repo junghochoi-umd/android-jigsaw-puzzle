84884ae37a0d4ce4ff4b3a8bc702b83387c43ed6
dcdaveman1 <davidmchoi19@gmail.com>
2022-11-15 13:20:00 -0500

    firebase

--- app/src/main/java/com/example/jigsaw_puzzle/MainActivity.kt
+++ app/src/main/java/com/example/jigsaw_puzzle/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.jigsaw_puzzle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun logout(view: View) {
+        FirebaseAuth.getInstance().signOut()
+        intent = Intent(applicationContext, Login::class.java)
+        startActivity(intent)
+
     }
 }
