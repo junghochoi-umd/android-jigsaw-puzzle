bc0ad24a8079a63defc38a6e15ee36c932b680e3
dchoi315 <dchoi315@umd.edu>
2022-11-14 20:47:21 -0500

    frontend somewhat finished'

---
+++ app/src/main/java/com/example/jigsaw_puzzle/HomePageActivity.kt
@@ -0,0 +1,17 @@
+package com.example.jigsaw_puzzle
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class HomePageActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+
+    }
+
+
+}
--- app/src/main/java/com/example/jigsaw_puzzle/MainActivity.kt
+++ app/src/main/java/com/example/jigsaw_puzzle/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.jigsaw_puzzle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val loginButton: Button = findViewById(R.id.login_button)
+
+        loginButton.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, HomePageActivity::class.java)
+            startActivity(intent)
+        })
+
     }
 }
