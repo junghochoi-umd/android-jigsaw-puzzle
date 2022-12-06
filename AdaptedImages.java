2b48c1900ef62d13be4f2ecfb36c43688a7d6ebd
aadria <aadriab+@gmail.com>
2022-12-05 20:32:34 -0500

    updated

--- AdaptedImages.java
+++ AdaptedImages.java
@@ -16,22 +16,26 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ImageAdapter extends BaseAdapter {
-    private Context mContext;
-    private AssetManager am;
-    private String[] files;
+    
+    private Context con;
+    private AssetManager manager;
+    private String[] saved;
 
     public ImageAdapter(Context c) {
-        mContext = c;
-        am = mContext.getAssets();
+
+        con = c;
+        manager = con.getAssets();
+
+        //adding images to file
         try {
-            files  = am.list("img");
+            saved  = manager.list("img");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public int getCount() {
-        return files.length;
+        return saved.length;
     }
 
     public Object getItem(int position) {
@@ -42,69 +46,76 @@ public class ImageAdapter extends BaseAdapter {
         return 0;
     }
 
-    // create a new ImageView for each item referenced by the Adapter
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
-            convertView = layoutInflater.inflate(R.layout.grid_element, null);
+
+    public View getView(final int position, View changeview, ViewGroup parent) {
+
+        if (changeview == null) {
+
+            final LayoutInflater layoutInflater = LayoutInflater.from(con);
+            changeview = layoutInflater.inflate(R.layout.grid_element, null);
         }
 
-        final ImageView imageView = convertView.findViewById(R.id.gridImageview);
+        final ImageView imageView = changeview.findViewById(R.id.gridImageview);
         imageView.setImageBitmap(null);
-        // run image related code after the view was laid out
+
+        //created the view
+
         imageView.post(new Runnable() {
             @Override
             public void run() {
+
                 new AsyncTask<Void, Void, Void>() {
-                    private Bitmap bitmap;
+
+                    private Bitmap map;
                     @Override
-                    protected Void doInBackground(Void... voids) {
-                        bitmap = getPicFromAsset(imageView, files[position]);
+
+                    protected Void back(Void... voids) {
+
+                        map = getPicFromAsset(imageView, saved[position]);
                         return null;
                     }
 
                     @Override
-                    protected void onPostExecute(Void aVoid) {
-                        super.onPostExecute(aVoid);
-                        imageView.setImageBitmap(bitmap);
+                    protected void onPostExecute(Void v) {
+                        super.onPostExecute(v);
+                        imageView.setImageBitmap(map);
                     }
                 }.execute();
             }
         });
 
-        return convertView;
+        return changeview;
     }
 
     private Bitmap getPicFromAsset(ImageView imageView, String assetName) {
-        // Get the dimensions of the View
-        int targetW = imageView.getWidth();
-        int targetH = imageView.getHeight();
 
-        if(targetW == 0 || targetH == 0) {
-            // view has no dimensions set
+        int viewWidth = imageView.getWidth();
+        int viewHeight = imageView.getHeight();
+
+        if(viewWidth == 0 || viewHeight == 0) {
             return null;
         }
 
         try {
-            InputStream is = am.open("img/" + assetName);
-            // Get the dimensions of the bitmap
-            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
-            bmOptions.inJustDecodeBounds = true;
-            BitmapFactory.decodeStream(is, new Rect(-1, -1, -1, -1), bmOptions);
-            int photoW = bmOptions.outWidth;
-            int photoH = bmOptions.outHeight;
 
-            // Determine how much to scale down the image
-            int scaleFactor = Math.min(photoW/targetW, photoH/targetH);
+            InputStream is = manager.open("img/" + assetName);
+            BitmapFactory.Options options = new BitmapFactory.Options();
 
+            options.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(is, new Rect(-1, -1, -1, -1), options);
+
+            int imageWidth = options.outWidth;
+            int imageHeight = options.outHeight;
+
+            int scaleFactor = Math.min(imageWidth/viewWidth, imageHeight/viewHeight);
             is.reset();
 
-            // Decode the image file into a Bitmap sized to fill the View
-            bmOptions.inJustDecodeBounds = false;
-            bmOptions.inSampleSize = scaleFactor;
-            bmOptions.inPurgeable = true;
+            options.inJustDecodeBounds = false;
+            options.inSampleSize = scaleFactor;
+            options.inPurgeable = true;
 
-            return BitmapFactory.decodeStream(is, new Rect(-1, -1, -1, -1), bmOptions);
+            return BitmapFactory.decodeStream(is, new Rect(-1, -1, -1, -1), options);
+            
         } catch (IOException e) {
             e.printStackTrace();
 
